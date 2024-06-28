c1bac9a3628997009db8a4d0d88f89f662526821
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-06-28 10:25:48 +0300

    Working on Arrow Functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function(){
+    const a = 2000;
+    const b = 100;
+    const divide = a/b;
+return(divide);
+}
+const square = () => {
+    let x= 2;
+    return(x*x)
+}
+const add = () => {
+    let a = 3;
+    let b = 4;
+
+    return a + b;
+}
